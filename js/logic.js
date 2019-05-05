53faf3a1c5a0e2b67288f92a7b361289d187652c
willow <shaneweth77@gmail.com>
2019-05-04 20:14:16 -0400

    added listener to the voiceDropdown

--- js/logic.js
+++ js/logic.js
@@ -8,4 +8,27 @@ const stopButton = document.querySelector("#stop");
 
 msg.text = document.querySelector('[name="text"]').value
 
+function populateVoices() {
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+        .join("");
+}
 
+function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+}
+
+function toggle(startOver = true) {
+    speechSynthesis.cancel();
+    if(startOver) {
+        speechSynthesis.speak(msg);
+    }
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+
+
+
+options.forEach(option => option.addEventListener('change', setOption));
